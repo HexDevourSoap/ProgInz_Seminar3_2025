528f5b3cbdf09c436f6f9b25441dbc2aa3ceb85c
HexDevourSoap <forgottenbabo@gmail.com>
2025-04-04 17:03:32 +0300

    seminar 3 Customer model added

---
+++ src/main/java/lv/venta/model/Customer.java
@@ -0,0 +1,30 @@
+package lv.venta.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lv.venta.model.enums.City;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "CustomerAsCompany") // MYSQL - Address_table
+@Entity
+public class Customer {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
