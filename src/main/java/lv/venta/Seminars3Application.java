f908f091eacc566330f8d78a6295e15d603f6b5c
HexDevourSoap <forgottenbabo@gmail.com>
2025-04-04 16:27:51 +0300

    seminar 3

---
+++ src/main/java/lv/venta/Seminars3Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminars3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminars3Application.class, args);
+	}
+
+}
